764cb6fc6418096776edf04d87dc91b06d16b7c9
pozapei <lukas369894@gmail.com>
2023-09-23 12:25:08 -0300

    Dog generator (final)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+// stay with index.css/js, app.js, remove reportWebVitals and his function from index.js,
+// strictMode from index.js, imports on app.js/index.js and clean index.css
+import { useEffect, useState } from 'react';
+import './index.css'
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+
+
 
 function App() {
+
+  const [dogImg, setDogImg] = useState('')
+
+  const getDogImg = async () => {
+    const response = await fetch('https://dog.ceo/api/breeds/image/random')
+    const dog = await response.json()
+    return dog.message
+  }
+
+  const generateDog = async () => {
+    setDogImg(await getDogImg())
+  }
+  useEffect(() => {
+    generateDog()
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='container'>
+        <div className="style-content">
+
+          <h1><strong className='btn' onClick={generateDog}>CLICK</strong> TO SEE A NEW DOG</h1>
+          <div className='img-div'>
+            <LazyLoadImage alt="" src={dogImg}  loading='lazy'/>
+          </div>
+        </div>
+      </div>
+
+
+    </>
   );
 }
 
